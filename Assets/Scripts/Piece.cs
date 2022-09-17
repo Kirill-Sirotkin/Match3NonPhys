fb24b9ef690b12359ef260ad86f33334cb7f7b14
Samcolserra <samcolserra@gmail.com>
2022-09-17 23:56:46 +0300

    Piece highlight

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -5,15 +5,17 @@ using DG.Tweening;
 
 namespace Match3NonPhys
 {
-    public class Piece : MonoBehaviour
+    public class Piece : MonoBehaviour, IClickable
     {
-        [field: SerializeField] private float _moveTime;
+        [field: SerializeField] private GameObject _highlight;
+        [field: SerializeField] private GameObject _visual;
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
-                Move(new Vector3(transform.position.x, transform.position.y - 3f, 0f));
+                //Move(new Vector3(transform.position.x, transform.position.y - 3f, 0f));
+                Spin();
             }
             if (Input.GetKeyDown(KeyCode.S))
             {
@@ -23,7 +25,23 @@ namespace Match3NonPhys
 
         public void Move(Vector3 pos)
         {
-            transform.DOMove(pos, _moveTime).SetEase(Ease.OutBack);
+            transform.DOMove(pos, Random.Range(0.72f, 0.77f)).SetEase(Ease.OutBack);
+        }
+        public void Spin()
+        {
+            _visual.transform.DORotate(new Vector3(0f, 360f, 0f), 0.25f, RotateMode.FastBeyond360);
+        }
+        public void ToggleHighlight(bool b)
+        {
+            _highlight.SetActive(b);
+        }
+        public void ToggleHighlight()
+        {
+            _highlight.SetActive(!_highlight.activeSelf);
+        }
+        public void ClickAction()
+        {
+            ToggleHighlight();
         }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Match3NonPhys
+{
+    public class Player : MonoBehaviour
+    {
+        private Piece _selectedPiece;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                MouseRay();
+            }
+        }
+
+        private void MouseRay()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (!Physics.Raycast(ray, out hit, 100)) { return; }
+            IClickable clickable = hit.transform.GetComponent<IClickable>();
+
+            if (clickable == null) { return; }
+            clickable.ClickAction();
+
+            
+        }
+    }
+
+}
