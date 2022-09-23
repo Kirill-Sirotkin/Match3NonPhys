083bb2e1a2a3455d34fc30e13f7a566f2ddd907d
Samcolserra <samcolserra@gmail.com>
2022-09-23 20:51:47 +0300

    Update Player.cs (shortened)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,27 +8,12 @@ namespace Match3NonPhys
     public class Player : MonoBehaviour
     {
         [field: SerializeField] private GameManager _manager;
-        private Piece _selectedPiece;
-
-        private List<Piece> _pieces = new List<Piece>();
-        private bool done = false;
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
                 MouseRay();
-            }
-            if(_pieces.Count >= 3)
-            {
-                if (!done)
-                {
-                    done = true;
-                    foreach (Piece p in _pieces)
-                    {
-                        p.Spin();
-                    }
-                }
             }
         }
 
@@ -39,26 +24,6 @@ namespace Match3NonPhys
 
             if (!Physics.Raycast(ray, out hit, 100)) { return; }
             _manager.PassSelection(hit);
-        }
-        private void SwapPieces(Piece p)
-        {
-            if (Vector3.Distance(p.transform.position, _selectedPiece.transform.position) > 1.1f)
-            {
-                p.ToggleHighlight(false);
-
-                _selectedPiece.ToggleHighlight(false);
-                _selectedPiece = null;
-                return;
-            }
-
-            Vector3 pos = p.transform.position;
-
-            p.ToggleHighlight(false);
-            p.Move(_selectedPiece.transform.position);
-
-            _selectedPiece.Move(pos);
-            _selectedPiece.ToggleHighlight(false);
-            _selectedPiece = null;
         }
     }
 
