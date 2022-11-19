b450e5ee00dd295ba1ccce545313c55d7890b6de
Samcolserra <samcolserra@gmail.com>
2022-11-20 00:52:07 +0200

    Pattern class started

---
+++ Assets/Scripts/Pattern.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Match3NonPhys
+{
+    public class Pattern
+    {
+        public List<Piece> _piecesInPattern { get; private set; }
+
+
+    }
+}
