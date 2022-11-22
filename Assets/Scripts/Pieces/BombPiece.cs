88899478179b74b7220729616f9e87f9f8387577
Samcolserra <samcolserra@gmail.com>
2022-11-22 18:38:45 +0200

    Update BombPiece.cs
    Special animation for Bomb working.

--- Assets/Scripts/Pieces/BombPiece.cs
+++ Assets/Scripts/Pieces/BombPiece.cs
@@ -15,18 +15,44 @@ namespace Match3NonPhys
         }
         public new void ClickAction()
         {
-            Debug.Log("Special piece activated: Bomb piece, " + transform.position);
-            Debug.Log("Pieces to despawn: ");
-
-            foreach (Piece p in SpecialMovePieces())
-            {
-                Debug.Log(p.transform.position);
-            }
-            Debug.Log("------------");
+            ToggleHighlight();
+            SpecialMoveAnimation();
         }
         public Sequence SpecialMoveAnimation()
         {
             Sequence seq = DOTween.Sequence();
+
+            transform.DOKill();
+            transform.localScale = transform.parent.localScale;
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
+
+            seq.Join(transform.DOMoveZ(-0.25f, 0.001f));
+            seq.Join(ScaleAnimation(0.75f, 3f));
+            seq.Append(ScaleAnimation(3f, 0.75f));
+            seq.Append(transform.DOMoveZ(0f, 0.001f));
+
+            return seq;
+        }
+        private Sequence ScaleAnimation(float horizontalScale, float verticalScale)
+        {
+            Sequence seq = DOTween.Sequence();
+
+            Vector3 startScale = transform.localScale;
+
+            seq.Join(transform.DOScale(
+                new Vector3(
+                    transform.localScale.x * horizontalScale, 
+                    transform.localScale.y * verticalScale, 
+                    transform.localScale.z), 
+                0.125f));
+
+            seq.Append(transform.DOScale(
+                new Vector3(
+                    startScale.x,
+                    startScale.y,
+                    startScale.z),
+                0.125f));
+
             return seq;
         }
         public List<Piece> SpecialMovePieces()
