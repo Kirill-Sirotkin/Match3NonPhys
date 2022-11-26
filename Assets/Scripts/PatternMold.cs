d603c0be8feb40dff50d63db994ff2c6d7099660
Samcolserra <samcolserra@gmail.com>
2022-11-26 19:42:05 +0200

    Update PatternMold.cs
    PatternMold class V1 done.

--- Assets/Scripts/PatternMold.cs
+++ Assets/Scripts/PatternMold.cs
@@ -6,21 +6,85 @@ namespace Match3NonPhys
 {
     public class PatternMold : MonoBehaviour
     {
-        public List<Vector3> _pieceCoords { get; private set; }
+        public List<List<Vector3>> _molds { get; private set; }
+        private List<Vector3> _patternCoords;
 
-        public PatternMold(List<Vector3> pieceCoordinates)
+        public PatternMold(List<Vector3> patternCoordinates)
         {
-            _pieceCoords = pieceCoordinates;
+            _patternCoords = patternCoordinates;
+
+            _molds = GeneratePatternMolds(_patternCoords);
         }
-        public PatternMold(Vector3 pieceCoordinate)
+        public PatternMold(Vector3 patternCoordinate)
         {
-            _pieceCoords = new List<Vector3>();
-            _pieceCoords.Add(pieceCoordinate);
+            _patternCoords = new List<Vector3>();
+            _patternCoords.Add(patternCoordinate);
+
+            _molds = GeneratePatternMolds(_patternCoords);
+        }
+        public PatternMold(Vector3[] patternCoordinates)
+        {
+            _patternCoords = new List<Vector3>();
+            foreach(Vector3 coordinate in patternCoordinates)
+            {
+                _patternCoords.Add(coordinate);
+            }
+
+            _molds = GeneratePatternMolds(_patternCoords);
+        }
+
+        private List<List<Vector3>> GeneratePatternMolds(List<Vector3> originalMold)
+        {
+            List<List<Vector3>> patternMolds = new List<List<Vector3>>();
+
+            List<Vector3> mirroredOriginalMold = MirrorPattern(originalMold);
+            for (int i = 0; i < 4; i++)
+            {
+                patternMolds.Add(RotatePattern(originalMold, i));
+                patternMolds.Add(RotatePattern(mirroredOriginalMold, i));
+            }
+
+            return patternMolds;
         }
 
         private List<Vector3> MirrorPattern(List<Vector3> patternToMirror)
         {
-            return new List<Vector3>();
+            return MirrorPattern(patternToMirror, true, false);
+        }
+        private List<Vector3> MirrorPattern(List<Vector3> patternToMirror, bool xCoordinateMirror, bool yCoordinateMirror)
+        {
+            List<Vector3> mirroredPattern = new List<Vector3>();
+            int xMultiplier = xCoordinateMirror ? -1 : 1;
+            int yMultiplier = yCoordinateMirror ? -1 : 1;
+
+            foreach(Vector3 coordInPattern in patternToMirror)
+            {
+                mirroredPattern.Add(new Vector3(
+                    coordInPattern.x * xMultiplier,
+                    coordInPattern.y * yMultiplier,
+                    coordInPattern.z));
+            }
+
+            return mirroredPattern;
+        }
+
+        private List<Vector3> RotatePattern(List<Vector3> patternToRotate, int numberOfRotations)
+        {
+            List<Vector3> rotatedPattern = new List<Vector3>();
+
+            foreach (Vector3 coordInPattern in patternToRotate)
+            {
+                Vector3 rotatedCoordinate = new Vector3(coordInPattern.x, coordInPattern.y, coordInPattern.z);
+
+                for (int i = 0; i < numberOfRotations; i++)
+                {
+                    rotatedCoordinate = new Vector3(rotatedCoordinate.y, rotatedCoordinate.x * -1, rotatedCoordinate.z);
+                }
+
+                rotatedPattern.Add(rotatedCoordinate);
+            }
+
+            return rotatedPattern;
         }
     }
 }
